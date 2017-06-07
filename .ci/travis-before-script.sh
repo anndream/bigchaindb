3f061d097ecd588e96c0a8b4a414d83d5628d5c0
krish7919 (Krish) <krishnan.k.iyer@gmail.com>
2017-06-08 01:21:55 +0200

    adding markers

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -13,6 +13,9 @@ elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb-ssl ]]; then
     wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-3.4.4.tgz -O /tmp/mongodb-ssl.tgz
     tar -xvf /tmp/mongodb-ssl.tgz
     mkdir /tmp/mongodb-ssl-data
+    echo "marker1"
+    sudo netstat -nalp 
+    sudo ps -ef
     ${PWD}/mongodb-linux-x86_64-3.4.4/bin/mongod \
         --dbpath=/tmp/mongodb-ssl-data \
         --replSet=bigchain-rs \
@@ -21,4 +24,8 @@ elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb-ssl ]]; then
         --sslCAFile=$TRAVIS_BUILD_DIR/tests/backend/mongodb-ssl/certs/ca.crt \
         --sslCRLFile=$TRAVIS_BUILD_DIR/tests/backend/mongodb-ssl/certs/crl.pem \
         --sslPEMKeyFile=$TRAVIS_BUILD_DIR/tests/backend/mongodb-ssl/certs/test_mdb_ssl_cert_and_key.pem &> /dev/null &
+    echo "marker2"
+    sudo netstat -nalp
+    sudo netstat -nalp
+    sudo ps -ef
 fi
